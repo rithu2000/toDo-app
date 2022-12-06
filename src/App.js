513555da8157a80ee4acaa45af505a52edc10a66
RITHU M <rithunlr1@gmail.com>
2022-12-06 11:29:14 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -26,8 +26,15 @@ function App() {
               <div className="todo">
                 <div className="left">
                   <input onChange={(e) => { 
-                    console.log(e.target.value); 
+                    console.log(e.target.checked); 
                     console.log(obj);
+                    setToDos(toDos.filter(obj2 => {
+              if (obj2.id === obj.id) {
+                obj2.status = e.target.checked 
+                
+              } 
+              return obj2       
+                    }))
                     }} value={obj.status} type="checkbox" name="" id="" />
                   <p>{obj.text}</p>
                 </div>
